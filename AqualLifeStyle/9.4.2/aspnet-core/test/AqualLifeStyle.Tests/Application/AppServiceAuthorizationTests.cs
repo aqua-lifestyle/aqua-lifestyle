45a32a5197b69c31cc66e305d96835481071fb10
Xolani-M <nathanielmvana@gmail.com>
2026-07-12 01:24:36 +0200

    test(authorization): extend AppServiceAuthorizationTests with newly authorized services
    Assert class- and method-level AbpAuthorize declarations for
    CustomerAppService, MembershipAppService, MembershipBenefitAppService,
    EnquiryAppService, OrderIntentAppService, and ProductAppService to
    prevent silent auth regression.

--- AqualLifeStyle/9.4.2/aspnet-core/test/AqualLifeStyle.Tests/Application/AppServiceAuthorizationTests.cs
+++ AqualLifeStyle/9.4.2/aspnet-core/test/AqualLifeStyle.Tests/Application/AppServiceAuthorizationTests.cs
@@ -3,7 +3,12 @@ using System.Linq;
 using System.Reflection;
 using Abp.Authorization;
 using AqualLifeStyle.Application.AreaLeaders;
+using AqualLifeStyle.Application.Customers;
+using AqualLifeStyle.Application.Enquiries;
 using AqualLifeStyle.Application.Facilitators;
+using AqualLifeStyle.Application.Memberships;
+using AqualLifeStyle.Application.Orders;
+using AqualLifeStyle.Application.Products;
 using AqualLifeStyle.Application.Referrals;
 using AqualLifeStyle.Authorization;
 using Shouldly;
@@ -46,6 +51,64 @@ namespace AqualLifeStyle.Tests.Application
         {
             AssertAuthorizeAttribute(typeof(ReferralAppService), PermissionNames.Pages_Referrals);
             AssertAuthorizeAttribute(typeof(ReferralAppService), nameof(ReferralAppService.ConfirmAwardAsync), PermissionNames.Pages_Referrals_Manage);
+        }
+
+        [Fact]
+        public void CustomerAppService_ShouldRequireCustomerPermissions()
+        {
+            AssertAuthorizeAttribute(typeof(CustomerAppService), PermissionNames.Pages_Customers);
+            AssertAuthorizeAttribute(typeof(CustomerAppService), nameof(CustomerAppService.CreateAsync), PermissionNames.Pages_Customers_Manage);
+            AssertAuthorizeAttribute(typeof(CustomerAppService), nameof(CustomerAppService.UpdateAsync), PermissionNames.Pages_Customers_Manage);
+        }
+
+        [Fact]
+        public void MembershipAppService_ShouldRequireMembershipPermissions()
+        {
+            AssertAuthorizeAttribute(typeof(MembershipAppService), PermissionNames.Pages_Memberships);
+            AssertAuthorizeAttribute(typeof(MembershipAppService), nameof(MembershipAppService.CreateAsync), PermissionNames.Pages_Memberships_Manage);
+            AssertAuthorizeAttribute(typeof(MembershipAppService), nameof(MembershipAppService.UpdateAsync), PermissionNames.Pages_Memberships_Manage);
+            AssertAuthorizeAttribute(typeof(MembershipAppService), nameof(MembershipAppService.SetActivationDateAsync), PermissionNames.Pages_Memberships_Manage);
+            AssertAuthorizeAttribute(typeof(MembershipAppService), nameof(MembershipAppService.SetMonthlyObligationAsync), PermissionNames.Pages_Memberships_Manage);
+            AssertAuthorizeAttribute(typeof(MembershipAppService), nameof(MembershipAppService.MarkObligationMetAsync), PermissionNames.Pages_Memberships_Manage);
+        }
+
+        [Fact]
+        public void MembershipBenefitAppService_ShouldRequireMembershipBenefitPermissions()
+        {
+            AssertAuthorizeAttribute(typeof(MembershipBenefitAppService), PermissionNames.Pages_MembershipBenefits);
+            AssertAuthorizeAttribute(typeof(MembershipBenefitAppService), nameof(MembershipBenefitAppService.CreateAsync), PermissionNames.Pages_MembershipBenefits_Manage);
+            AssertAuthorizeAttribute(typeof(MembershipBenefitAppService), nameof(MembershipBenefitAppService.UpdateAsync), PermissionNames.Pages_MembershipBenefits_Manage);
+            AssertAuthorizeAttribute(typeof(MembershipBenefitAppService), nameof(MembershipBenefitAppService.DeleteAsync), PermissionNames.Pages_MembershipBenefits_Manage);
+        }
+
+        [Fact]
+        public void EnquiryAppService_ShouldRequireEnquiryPermissions()
+        {
+            AssertAuthorizeAttribute(typeof(EnquiryAppService), PermissionNames.Pages_Enquiries);
+            AssertAuthorizeAttribute(typeof(EnquiryAppService), nameof(EnquiryAppService.CreateAsync), PermissionNames.Pages_Enquiries_Manage);
+            AssertAuthorizeAttribute(typeof(EnquiryAppService), nameof(EnquiryAppService.RespondAsync), PermissionNames.Pages_Enquiries_Manage);
+            AssertAuthorizeAttribute(typeof(EnquiryAppService), nameof(EnquiryAppService.CloseAsync), PermissionNames.Pages_Enquiries_Manage);
+            AssertAuthorizeAttribute(typeof(EnquiryAppService), nameof(EnquiryAppService.ReopenAsync), PermissionNames.Pages_Enquiries_Manage);
+            AssertAuthorizeAttribute(typeof(EnquiryAppService), nameof(EnquiryAppService.AssignToMemberAsync), PermissionNames.Pages_Enquiries_Manage);
+            AssertAuthorizeAttribute(typeof(EnquiryAppService), nameof(EnquiryAppService.ConvertToCustomerAsync), PermissionNames.Pages_Enquiries_Manage);
+            AssertAuthorizeAttribute(typeof(EnquiryAppService), nameof(EnquiryAppService.ClearAssignmentAsync), PermissionNames.Pages_Enquiries_Manage);
+            AssertAuthorizeAttribute(typeof(EnquiryAppService), nameof(EnquiryAppService.RecordFollowUpAsync), PermissionNames.Pages_Enquiries_Manage);
+        }
+
+        [Fact]
+        public void OrderIntentAppService_ShouldRequireOrderPermissions()
+        {
+            AssertAuthorizeAttribute(typeof(OrderIntentAppService), PermissionNames.Pages_Orders);
+            AssertAuthorizeAttribute(typeof(OrderIntentAppService), nameof(OrderIntentAppService.CreateFromEnquiryAsync), PermissionNames.Pages_Orders_Manage);
+            AssertAuthorizeAttribute(typeof(OrderIntentAppService), nameof(OrderIntentAppService.CancelAsync), PermissionNames.Pages_Orders_Manage);
+            AssertAuthorizeAttribute(typeof(OrderIntentAppService), nameof(OrderIntentAppService.CompleteAsync), PermissionNames.Pages_Orders_Manage);
+        }
+
+        [Fact]
+        public void ProductAppService_ShouldRequireProductPermissions()
+        {
+            AssertAuthorizeAttribute(typeof(ProductAppService), PermissionNames.Pages_Products);
+            AssertAuthorizeAttribute(typeof(ProductAppService), nameof(ProductAppService.CreateAsync), PermissionNames.Pages_Products_Manage);
         }
 
         private static void AssertAuthorizeAttribute(Type serviceType, string permissionName)
